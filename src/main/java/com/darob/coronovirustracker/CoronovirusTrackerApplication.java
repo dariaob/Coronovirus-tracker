3f50713bdcb0632e99844a36774ee7b1190068f0
Daria Obukhova <pandazheya@gmail.com>
2021-06-30 19:25:25 +0500

    Added csv reading method

---
+++ src/main/java/com/darob/coronovirustracker/CoronovirusTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.darob.coronovirustracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronovirusTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoronovirusTrackerApplication.class, args);
+    }
+
+}
