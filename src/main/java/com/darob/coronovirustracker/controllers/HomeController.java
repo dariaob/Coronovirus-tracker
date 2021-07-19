508aa42f0e5613313d5f8da21abf8be1eb4b873f
dariaob <pandazheya@gmail.com>
2021-07-19 22:33:32 +0500

    Added CSS styles, numbers of previous day cases. Changed HomeController class: 1.Added 3 new variables 2.Added stream() method to this variables

--- src/main/java/com/darob/coronovirustracker/controllers/HomeController.java
+++ src/main/java/com/darob/coronovirustracker/controllers/HomeController.java
@@ -1,10 +1,13 @@
 package com.darob.coronovirustracker.controllers;
 
 import com.darob.coronovirustracker.services.CoronavirusDataService;
+import models.LocationStats;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
@@ -13,7 +16,12 @@ public class HomeController {
 
     @GetMapping(path = "/")
     public String home(Model model) {
-        model.addAttribute("locationStats", coronavirusDataService.getAllStates());
+        List<LocationStats> allStates = coronavirusDataService.getAllStates();
+        int totalReportedCases = allStates.stream().mapToInt(stat -> stat.getCasesTotal()).sum();
+        int totalNewCases = allStates.stream().mapToInt(stat -> stat.getDiffFromPrevDay()).sum();
+        model.addAttribute("locationStats", allStates);
+        model.addAttribute("totalReportedCases", totalReportedCases);
+        model.addAttribute("totalNewCases", totalNewCases);
         return "home";
     }
 }
